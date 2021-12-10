3a6667cb2800e8a103adf531c0c424fbc205ba1b
aizhan-ken <aizhanlee93@gmail.com>
2021-12-10 01:53:17 -0500

    try for the milestone

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "idk", "Student", "Sleep deprived"],
+        strings: ["Developer", "Designer", "idk", "Sleep deprived"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
